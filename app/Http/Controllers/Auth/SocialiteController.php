4a465b457a45dc481bd3476292139484729b966d
Mohammed <mohmmedlaeh81@gmail.com>
2024-02-24 17:02:02 +0100

    login with googel and github

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -1,22 +1,38 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-use Laravel\Socialite\Facades\Socialite;
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
     public function redirect($Socialite)
     {
-        return Socialite::driver($Socialite)->redirect();
+       return Socialite::driver('github')->redirect();
+     
 
     }
 
     public function callback($Socialite){
-        $user = Socialite::driver($Socialite)->user();
-        dd( $user);
-
+        $githubUser = Socialite::driver('github')->user();
+    
+        $user = User::updateOrCreate([
+            'id' => $githubUser->id,
+        ], [
+            'name' => $githubUser->name,
+            'email' => $githubUser->email,
+            'github_token' => $githubUser->token,
+            'github_refresh_token' => $githubUser->refreshToken,
+            'password' => bcrypt('default'),
+        ]);
+    
+        //dd($githubUser);
+    
+        auth()->login($user);
+    
+        return redirect('/dashboard');
     }
     
 }
